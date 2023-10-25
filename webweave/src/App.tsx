8c316afb8ff71e1ecfcac8e39627686209a0a8e1
ofisch <onni.fischer@gmail.com>
2023-10-25 14:15:58 +0300

    init

---
+++ webweave/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const style = {
+  header: "text-3xl font-bold underline",
+};
+
+function App() {
+  return <h1 className={style.header}>Hello world!</h1>;
+}
+
+export default App;
